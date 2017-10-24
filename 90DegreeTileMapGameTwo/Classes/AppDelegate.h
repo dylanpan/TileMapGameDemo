6a56e305a0378926f1d94655013cb2c8edc2009d
dylanpan <dylanpan@live.com>
2017-10-24 13:59:11 +0800

    Initial Commit

---
+++ 90DegreeTileMapGameTwo/Classes/AppDelegate.h
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.h
+//
+//  Created by : 潘安宇
+//  Project    : 90DegreeTileMapGameTwo
+//  Date       : 2017/10/24
+//
+//  Copyright (c) 2017年 潘安宇.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+// -----------------------------------------------------------------------
+
+@interface AppDelegate : CCAppDelegate
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
